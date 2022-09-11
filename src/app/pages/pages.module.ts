c506cc0093f9e779895f5fdf9dd59aef10f2cc6c
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-11 17:39:09 -0300

    header, search and list added

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,12 @@ import { CommonModule } from '@angular/common'
 import { PagesRoutingModule } from './routing.module'
 
 // Pages
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'
 import { DetailsComponent } from './details/details.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, PagesRoutingModule],
+  imports: [CommonModule, PagesRoutingModule, SharedModule],
 })
 export class PagesModule {}
